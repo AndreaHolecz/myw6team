3d8c6b0094280bba448484ebdaed45696d5c4ae3
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-03 14:46:27 +0200

    what

---
+++ frontend/public/script.js
@@ -0,0 +1,354 @@
+//Barna section
+const headerContent = () => {
+    return `
+    <header>
+    <nav>
+      <div>
+        <img src="" />
+      </div>
+      <div class="header-container">
+        <span>cruise color splash</span>
+        <div class="dropdown-1">
+          <span>shoes</span>
+          <div class="dropdown-content-1">
+            <ul>
+              <li>boots</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul>
+              <li>loafers</li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul>
+              <li>mules</li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul>
+              <li>sandals</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <ul>
+              <li>pumps</li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        <div class="dropdown-2">
+            <span>accessoires</span>
+            <div class="dropdown-content-2">
+              <ul>
+                <li>hip belts</li>
+              </ul>
+              <ul>
+                <li>waist belts</li>
+              </ul>
+              <ul>
+                <li>key hanger</li>
+              </ul>
+            </div>
+          </div>
+          <div class="dropdown-3">
+            <span>mariée</span>
+            <div class="dropdown-content-3">
+              <ul>
+                <li>impress the groom</li>
+              </ul>
+              <ul>
+                <li>halfway the party</li>
+              </ul>
+              <ul>
+                <li>the last dance</li>
+              </ul>
+            </div>
+          </div>
+          <div class="dropdown-4">
+            <span>maision</span>
+            <div class="dropdown-content-4">
+              <ul>
+                <li>tableware</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <ul>
+                <li>coffee & tea</li>
+                <li></li>
+                <li></li>
+              </ul>
+              <ul>
+                <li>vases</li>
+              </ul>
+              <ul>
+                <li>objects</li>
+                <li>bookstands</li>
+                <li>pose clés</li>
+                <li>sushi stick holder</li>
+              </ul>
+            </div>
+            </div>
+            <div class="dropdown-5">
+              <span>more</span>
+              <div class="dropdown-content-5">
+                <ul>
+                  <li>giftcard</li>
+                </ul>
+                <ul>
+                  <li>the world of</li>
+                </ul>
+                <ul>
+                  <li>stores</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="signs">
+        <span>dollar</span>
+        <span>prof</span>
+        <span>search</span>
+        <span>hearth</span>
+        <span>shopcart</span>
+      </div>
+    </nav>
+  </header>
+    `
+};
+
+const mainContent = () => {
+    return `
+    <article>
+        <div></div>
+    </article>  
+    `
+};
+
+//Barna section end
+
+//Florian section
+const swiperWrapper = () => {
+    return `
+    <div>
+        <h3>HERO STYLES</h3>
+        <div class="photo-block">
+            <div class="col1">
+                <a href="https://www.morobe.com/products/silver-sandals-high-heel-susan-01-3648">'img1'</a>
+             </div>
+             <div class="col2">
+                <a href="https://www.morobe.com/products/silver-sandals-high-heel-susan-01-3648">'img2'</a>
+            </div>
+            <div class="col3">
+                <a href="https://www.morobe.com/products/sunflower-sandals-high-heel-penny-10-3640">'img3'</a>
+            </div>
+            <div class="col4">
+                <a href="https://www.morobe.com/products/apple-pumps-high-heel-patty-03-3663">'img4'</a>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+//Lilla section start
+
+let newDAndR = () => {
+    return `
+    <div class="NewDropsAndRestocks">
+        <section class="NDR">
+            <div>
+                <div class="NDR_1">
+                    <div class="NDR_1_header">
+                        <h3>
+                        </h3>
+                    </div>
+
+                    <div class="NDR_1_image">
+                        <img>
+
+                        <span>
+                        </span>
+                    </div>
+
+                    <div class="NDR_1_hidden">
+                        <img>
+
+                        <span>
+                        </span>
+                    </div>
+
+                    <noscript>
+                    </noscript>
+                </div>
+
+                <div class="NDR_2">
+                    <div class"NDR_2_text">
+                        <p>
+                        </p>
+                    </div>
+
+                    <div class="NDR_2_button">
+                        <a>
+                            <span>shop new drops</span>
+                            <div>
+                                <div>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                    <span>shop new drops</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class"NDR_2_image">
+                        <img>
+                        <span> 
+                        </span>
+                    </div>
+
+                    <noscript>
+                    </noscript>
+                </div>
+
+                <div class="NDR_running_text">
+                    <a>
+                        <span>
+                        </span>
+
+                        <div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        <section>
+    </div>
+    `
+}
+
+//Lilla section end
+
+//Andi section
+const cruiseColor = () => {
+    return `
+    <div>
+        <section class="Section">
+            <divclas="FeatureTextSection4">
+                <div class="Content Wrapper">
+                    <div class="Content">
+                        <div class="Content head">
+                            <div class="Content--title">
+                            </div>
+                        </div>
+                        <div class="Content body">
+                            <div class="Content--subheading">
+                            </div>
+                            <div class="SectionHeader">
+                                <p></p>
+                            </div>
+                            <div class="Button Wrapper">
+                                <a>
+                                    <span></span>
+                                    <div class="marquee">
+                                        <div class="marquee_inner">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                    <div>
+                                </a>
+                            /</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ImageContairner_Wrapper">
+                    <h2></h2>
+                    <div>
+                        <img>
+                        <span></span>
+                        <noscript>
+                            <img>
+                        </noscript>
+                    </div>
+                    <div class="AspectRatio">
+                        <img>
+                        <span></span>
+                        <noscript>
+                            <img>
+                        </noscript>
+                    </div>
+                </div>
+            </divclas=>
+        </section>
+    </div>
+    `
+}
+//Andi section end
+
+const pictureContent = () => {
+    return `
+    <img src="">
+    `
+};
+//Kriszti section
+const footerContent = () => {
+    return `
+    <footer>
+        <div class="Container">
+
+            <div class="Footer-Inner>
+                <div class="Footer-Block Footer-Block-Logo>
+                    <div class="Footer__Block Footer__Block--links">
+                    <ul class="LinklistAsRow hidden-phone"><li class="Custom__Linklist__Item">
+                    <a href="/policies/shipping-policy" class="Link Link--underline">SHIPPING</a>
+                  </li><li class="Custom__Linklist__Item">
+                    <a href="/policies/refund-policy" class="Link Link--underline">RETURNS</a>
+                  </li><li class="Custom__Linklist__Item">
+                    <a href="/policies/terms-of-service" class="Link Link--underline">TERMS &amp; CONDITIONS</a>
+                  </li><li class="Custom__Linklist__Item">
+                    <a href="/policies/privacy-policy" class="Link Link--underline">PRIVACY POLICY</a>
+                  </li><li class="Custom__Linklist__Item">
+                    <a href="/pages/contact" class="Link Link--underline">CONTACT</a>
+                  </li><li class="Custom__Linklist__Item">
+                    <a href="/blogs/jobs" class="Link Link--underline">JOBS</a>
+                  </li></ul></div><div class="Footer__Block Footer__Block--text" style="flex-basis: 30%"><div class="Footer__Content--social-media Custom_Footer__Content--social-media">
+                <ul class=" HorizontalList HorizontalList--spacingLoose">
+
+
+    
+                    </div>
+                </div>
+            </div>
+         </div>
+    </footer>
+    `
+};
+//Kriszti section end
+
+
+
+const loadEvent = () => {
+    const rootEl = document.querySelector('#root');
+};
+
+window.addEventListener('load', loadEvent);
